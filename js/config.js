fafaa66779fa7622250be3123b29303c714e1230
Nur4ik00p <161104383+Nur4ik00p@users.noreply.github.com>
2025-05-07 10:31:16 +0000

    Обновил код скрипта

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// Configuration
-const API_URL = "https://demo.soon-night.lol"
-const SOCKET_URL = "https://demo.soon-night.lol"
-const FRONTEND_URL = "http://185.189.167.72:3000"
-
-// Storage keys
-const TOKEN_KEY = "soon_night_token"
-const USER_KEY = "soon_night_user"
-const THEME_KEY = "soon_night_theme"
